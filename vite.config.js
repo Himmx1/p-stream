eec8f58af9cf57cddcd858ef9978085374999681
Himmx1 <132150254+Himmx1@users.noreply.github.com>
2025-08-06 21:22:16 -0500

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist', // This specifies the output directory for the build
+  },
+});
